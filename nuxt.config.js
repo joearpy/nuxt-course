213f34720b3b50e1cd4c0dffe33c763652184967
joearpy <joearpy87@gmail.com>
2021-03-26 10:42:42 +0100

    Middleware & Authentication

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,75 +1,77 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'WD Blog',
+    title: "WD Blog",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My cool Web Development Blog' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "My cool Web Development Blog",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Open+Sans" }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Open+Sans",
+      },
+    ],
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fa923f', height: '4px', duration: 5000 },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fa923f", height: "4px", duration: 5000 },
   loadingIndicator: {
-    name: 'circle',
-    color: '#fa923f'
+    name: "circle",
+    color: "#fa923f",
   },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '~assets/styles/main.css'
-  ],
+   ** Global CSS
+   */
+  css: ["~assets/styles/main.css"],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~plugins/core-components.js',
-    '~plugins/date-filter.js'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["~plugins/core-components.js", "~plugins/date-filter.js"],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/axios',
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/axios"],
   axios: {
-    baseURL: process.env.BASE_URL || 'https://nuxt-blog.firebaseio.com',
-    credentials: false
+    baseURL: process.env.BASE_URL || "https://nuxt-blog.firebaseio.com",
+    credentials: false,
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
   },
   env: {
-    baseUrl: process.env.BASE_URL || 'https://nuxt-blog.firebaseio.com'
+    baseUrl: process.env.BASE_URL || "https://nuxt-blog.firebaseio.com",
+    fbAPIKey: "AIzaSyB4uttLQU61vyIppX2Tqgh447CNFuW7Vjo",
   },
   transition: {
-    name: 'fade',
-    mode: 'out-in'
-  }
-}
+    name: "fade",
+    mode: "out-in",
+  },
+  // router: {
+  //   middleware: 'log'
+  // }
+};
