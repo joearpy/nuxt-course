2940bda57a620020b00ff623d1fcef6782dde8cf
joearpy <joearpy87@gmail.com>
2021-03-26 11:04:00 +0100

    Add Server Side

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+const bodyParser = require("body-parser");
 
 module.exports = {
   mode: "universal",
@@ -74,4 +75,5 @@ module.exports = {
   // router: {
   //   middleware: 'log'
   // }
+  serverMiddleware: [bodyParser.json(), "~/api"],
 };
